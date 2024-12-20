e26e4d42e96cb2733949220a4f22413137eb89ac
Almeida <thierry.almeida@dxc.com>
2024-12-20 18:42:28 -0300

    First commit

---
+++ src/main/java/com/tralmeida/clients/CadastroDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.tralmeida.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tralmeida/clients/CadastroDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tralmeida.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
