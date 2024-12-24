65e837b76270971c0fb994ab684db903c0a5f01a
Thierry <thierryluiz@hotmail.com>
2024-12-23 21:28:59 -0300

    All methods and params for controller and service

--- src/main/java/com/tralmeida/clients/controllers/ClientController.java
+++ src/main/java/com/tralmeida/clients/controllers/ClientController.java
@@ -1,13 +1,18 @@
 package com.tralmeida.clients.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tralmeida.clients.dto.ClientDTO;
@@ -26,22 +31,39 @@ public class ClientController {
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<ClientDTO> findById(){
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		return null;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ClientDTO>> findByNome(@RequestParam(defaultValue = "") String nome){
+		return null;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ClientDTO>> findByEmail(@RequestParam(defaultValue = "") String email){
+		return null;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ClientDTO>> findByDtNascimentoBetWeen(
+			@RequestParam(defaultValue = "") String dtIni, 
+			@RequestParam(defaultValue = "") String dtFim){
 		return null;
 	}
 	
 	@PostMapping
-	public ResponseEntity<ClientDTO> create(){
+	public ResponseEntity<ClientDTO> create(@RequestBody ClientDTO dto){
 		return null;
 	}
 	
-	@PutMapping(value = "{id}")
-	public ResponseEntity<ClientDTO> update(){
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
 		return null;
 	}
 	
-	@DeleteMapping
-	public ResponseEntity<Void> delete(){
+	@DeleteMapping(value = "{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
 		return null;
 	}
 }
--- src/main/java/com/tralmeida/clients/services/ClientService.java
+++ src/main/java/com/tralmeida/clients/services/ClientService.java
@@ -1,9 +1,11 @@
 package com.tralmeida.clients.services;
 
-import java.awt.print.Pageable;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,27 +18,50 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
+	//JPA
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAll(Pageable pageable){
 		return null;
 	}
 	
+	//JPA
 	@Transactional(readOnly = true)
-	public ClientDTO findById() {
+	public ClientDTO findById(Long id) {
 		return null;
 	}
 	
+	//JDBC
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findByNome(String nome) {
+		return null;
+	}
+	
+	//Native query
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findByEmail(String email) {
+		return null;
+	}
+	
+	//Native query
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findByDtNascimentoBetween(Date dtIni, Date dtFim) {
+		return null;
+	}
+	
+	//JPA
 	@Transactional
-	public ClientDTO create() {
+	public ClientDTO create(ClientDTO dto) {
 		return null;
 	}
 	
+	//JDBC
 	@Transactional
-	public ClientDTO update() {
+	public ClientDTO update(Long id, ClientDTO dto) {
 		return null;
 	}
 	
-	public void delete() {
+	//JDBC
+	public void delete(Long id) {
 		return;
 	}
 }
