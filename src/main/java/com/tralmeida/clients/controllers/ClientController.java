80901881a44e4271c05ce693b7906e43e0641671
Thierry <thierryluiz@hotmail.com>
2024-12-27 02:17:15 -0300

    All controller methods

--- src/main/java/com/tralmeida/clients/controllers/ClientController.java
+++ src/main/java/com/tralmeida/clients/controllers/ClientController.java
@@ -1,5 +1,6 @@
 package com.tralmeida.clients.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tralmeida.clients.dto.ClientDTO;
 import com.tralmeida.clients.services.ClientService;
@@ -34,38 +36,46 @@ public class ClientController {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
-		return null;
+		ClientDTO result = service.findById(id);
+		return ResponseEntity.ok().body(result);
 	}
 	
 	@GetMapping(value = "/nome")
 	public ResponseEntity<List<ClientDTO>> findByNome(@RequestParam(defaultValue = "") String nome){
-		return null;
+		List<ClientDTO> result = service.findByNome(nome);
+		return ResponseEntity.ok().body(result);
 	}
 	
 	@GetMapping(value = "/email")
 	public ResponseEntity<List<ClientDTO>> findByEmail(@RequestParam(defaultValue = "") String email){
-		return null;
+		List<ClientDTO> result = service.findByEmail(email);
+		return ResponseEntity.ok().body(result);
 	}
 	
 	@GetMapping(value = "/dtNascimento")
 	public ResponseEntity<List<ClientDTO>> findByDtNascimentoBetWeen(
 			@RequestParam(defaultValue = "") String dtIni, 
 			@RequestParam(defaultValue = "") String dtFim){
-		return null;
+		List<ClientDTO> result = service.findByDtNascimentoBetween(dtIni, dtFim);
+		return ResponseEntity.ok().body(result);
 	}
 	
 	@PostMapping
 	public ResponseEntity<ClientDTO> create(@RequestBody ClientDTO dto){
-		return null;
+		dto = service.create(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 	
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
-		return null;
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
 	}
 	
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
-		return null;
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
