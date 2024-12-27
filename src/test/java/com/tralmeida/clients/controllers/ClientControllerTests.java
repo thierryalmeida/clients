5dc9ffa2e4b0fe26c2d7c8c520c790cb5fc9817a
Thierry <thierryluiz@hotmail.com>
2024-12-27 01:45:30 -0300

    Test method for endpoint create

--- src/test/java/com/tralmeida/clients/controllers/ClientControllerTests.java
+++ src/test/java/com/tralmeida/clients/controllers/ClientControllerTests.java
@@ -1,8 +1,11 @@
 package com.tralmeida.clients.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Date;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +17,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tralmeida.clients.dto.ClientDTO;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
@@ -22,15 +28,18 @@ public class ClientControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
 	
-	private Long existingId;
-	private Long nonExistingId;
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	private ClientDTO newClient;
 	private Integer countTotalClients;
+	private Long nextId;
 	
 	@BeforeEach
 	void setUp(){
-		existingId = 1L;
-		nonExistingId = 1000L;
 		countTotalClients = 50;
+		newClient = new ClientDTO(null, "Fulano", "fulano@teste.com", "40028922", new Date(), new Date());
+		nextId = 51L;
 	}
 	
 	@Test
@@ -42,5 +51,20 @@ public class ClientControllerTests {
 		result.andExpect(jsonPath("$.content").exists());
 		result.andExpect(jsonPath("$.content[0].nome").value("Alice Gomes"));
 		result.andExpect(jsonPath("$.content[1].nome").value("Ana Silva"));
+	}
+	
+	@Test
+	public void createShouldReturnNewClientDTOWithAttributesAndNewId() throws Exception {
+		String jsonBody = objectMapper.writeValueAsString(newClient);
+		ResultActions result = mockMvc.perform(post("/clients")
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isCreated());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.id").value(nextId));
+		result.andExpect(jsonPath("$.nome").value(newClient.getNome()));
+		result.andExpect(jsonPath("$.telefone").value(newClient.getTelefone()));
 	}
 }
