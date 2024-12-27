53004e8ec2bdfdec5508df5a5a8becfa187a6f98
Thierry <thierryluiz@hotmail.com>
2024-12-26 23:10:28 -0300

    Methods annotation in JPARepository

--- src/main/java/com/tralmeida/clients/repositories/ClientRepository.java
+++ src/main/java/com/tralmeida/clients/repositories/ClientRepository.java
@@ -1,11 +1,19 @@
 package com.tralmeida.clients.repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.tralmeida.clients.entities.Client;
 
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long>{
 	
+	List<Client> findByEmail(String email);
+	
+	List<Client> findByDtNascimentoBetween(Date dtIni, Date dtFim);
 }
